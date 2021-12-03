7d65717a9ba1af3ef65ebbe6fff829927fa6d652
Paula Lustosa <pl.lustosa@gmail.com>
2021-12-03 12:17:26 -0300

    Change in variables names

--- src/com/letscode/converter/Main.java
+++ src/com/letscode/converter/Main.java
@@ -3,18 +3,18 @@ package com.letscode.converter;
 public class Main {
 
     public static void main(String[] args) {
-        float userInputTemperatureInCelsius = 18f;
+        float temperatureInCelsius = 18f;
 
-        final float convertTemperatureCelsiusToFahrenheit = (userInputTemperatureInCelsius * 1.8f) + 32f;
-        final float convertTemperatureCelsiusToKelvin = userInputTemperatureInCelsius + 273.15f;
-        final float convertTemperatureCelsiusToReaumur = userInputTemperatureInCelsius * 0.8f;
-        final float convertTemperatureCelsiusToRankine = (userInputTemperatureInCelsius * 1.8f) + 491.67f;
+        final float temperatureInFahrenheit = (temperatureInCelsius * 1.8f) + 32f;
+        final float temperatureInKelvin = temperatureInCelsius + 273.15f;
+        final float temperatueInReaumur = temperatureInCelsius * 0.8f;
+        final float temperatureInRankine = (temperatureInCelsius * 1.8f) + 491.67f;
 
         System.out.printf("The temperature of %.2f °C is equal to: \n%.2f °F; \n%.2f K; \n%.2f °Ré or \n%.2f °R",
-                userInputTemperatureInCelsius,
-                convertTemperatureCelsiusToFahrenheit,
-                convertTemperatureCelsiusToKelvin,
-                convertTemperatureCelsiusToReaumur,
-                convertTemperatureCelsiusToRankine);
+                temperatureInCelsius,
+                temperatureInFahrenheit,
+                temperatureInKelvin,
+                temperatueInReaumur,
+                temperatureInRankine);
     }
 }
