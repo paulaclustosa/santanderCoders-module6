97b4da8cac7a51f2c56357609432119ca47e9c3f
Paula Lustosa <pl.lustosa@gmail.com>
2021-12-03 12:22:00 -0300

    Change in constants names

--- src/com/letscode/converter/Main.java
+++ src/com/letscode/converter/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args) {
         float temperatureInCelsius = 18f;
 
-        final float temperatureInFahrenheit = (temperatureInCelsius * 1.8f) + 32f;
-        final float temperatureInKelvin = temperatureInCelsius + 273.15f;
-        final float temperatueInReaumur = temperatureInCelsius * 0.8f;
-        final float temperatureInRankine = (temperatureInCelsius * 1.8f) + 491.67f;
+        final float TEMPERATURE_IN_FAHRENHEIT = (temperatureInCelsius * 1.8f) + 32f;
+        final float TEMPERATURE_IN_KELVIN = temperatureInCelsius + 273.15f;
+        final float TEMPERATURE_IN_REAUMUR = temperatureInCelsius * 0.8f;
+        final float TEMPERATURE_IN_RANKINE = (temperatureInCelsius * 1.8f) + 491.67f;
 
         System.out.printf("The temperature of %.2f °C is equal to: \n%.2f °F; \n%.2f K; \n%.2f °Ré or \n%.2f °R",
                 temperatureInCelsius,
-                temperatureInFahrenheit,
-                temperatureInKelvin,
-                temperatueInReaumur,
-                temperatureInRankine);
+                TEMPERATURE_IN_FAHRENHEIT,
+                TEMPERATURE_IN_KELVIN,
+                TEMPERATURE_IN_REAUMUR,
+                TEMPERATURE_IN_RANKINE);
     }
 }
