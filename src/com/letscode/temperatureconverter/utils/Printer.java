bd67b91b5878e59ef8959cc8598aabf308d8a55b
Paula Lustosa <pl.lustosa@gmail.com>
2021-12-05 23:44:19 -0300

    Refactoring

--- src/com/letscode/temperatureconverter/utils/Printer.java
+++ src/com/letscode/temperatureconverter/utils/Printer.java
@@ -5,11 +5,14 @@ import com.letscode.temperatureconverter.service.Conversion;
 public class Printer {
 
     public static void printEnterTemperatureMessage() {
-        System.out.print("Please enter the temperature in Celsius: ");
+        final String ENTER_TEMPERATURE_MESSAGE = "Please enter the temperature in Celsius: ";
+        System.out.print(ENTER_TEMPERATURE_MESSAGE);
     }
 
     public static void printConvertedTemperatureMessage(float temperatureInCelsius) {
-        System.out.printf("The temperature of %.2f °C is equal to: \n%.2f °F; \n%.2f K; \n%.2f °Ré or \n%.2f °R.",
+        final String CONVERTED_TEMPERATURE_MESSAGE = "The temperature of %.2f °C is equal to: " +
+                "\n%.2f °F; \n%.2f K; \n%.2f °Ré or \n%.2f °R.";
+        System.out.printf(CONVERTED_TEMPERATURE_MESSAGE,
                 temperatureInCelsius,
                 Conversion.convertToFahrenheit(temperatureInCelsius),
                 Conversion.convertToKelvin(temperatureInCelsius),
